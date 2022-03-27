61ec300f05dc8b71671d4cfc56444eca9a86b3ec
Adrianna Guevarra <adriannaguevarra@Adriannas-MacBook-Pro-Main.local>
2022-03-27 12:03:03 -0700

    update paragraph to say open doors sanctuary to test deployment workflow

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Open Doors Sanctuary
         </p>
         <a
           className="App-link"
