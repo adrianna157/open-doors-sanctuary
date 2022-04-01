7b7332c53688cdac30fdaf0aed615462caa9c660
Adrianna Guevarra <adriannaguevarra@Adriannas-MacBook-Pro-Main.local>
2022-04-01 08:17:39 -0700

    configure tailwind

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Open Doors Sanctuary
-        </p>
+        <p className="text-3xl font-bold underline">Open Doors Sanctuary</p>
         <a
           className="App-link"
           href="https://reactjs.org"
