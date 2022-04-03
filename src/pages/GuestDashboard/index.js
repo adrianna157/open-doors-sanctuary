89d346d134f71670d1dcc9bd123ead3e2b3c949a
Adrianna Guevarra <adriannaguevarra@Adriannas-MacBook-Pro-Main.local>
2022-04-03 05:26:58 -0700

    create search bar

---
+++ src/pages/GuestDashboard/index.js
@@ -0,0 +1,8 @@
+import SearchBar from "../../components/SearchBar"
+export default function GuestDashboard() {
+  return (
+      <div className="min-h-screen flex items-center justify-center bg-hacky-50">
+        <SearchBar/>
+      </div>
+  );
+}
--- src/pages/Header/index.js
+++ src/pages/Header/index.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import AuthContext from "../../auth/AuthProvider";
 import Confirm from "../../components/Confirm";
 import logo from "../../assets/open_doors_logo.svg";
+import HeaderText from "../../components/HeaderText";
 
 function Header() {
   const auth = useContext(AuthContext);
@@ -10,9 +11,19 @@ function Header() {
   return (
     <header className="absolute top-0 left-0 right-0 z-10">
       <div className="container mx-auto p-5 flex text-white">
-        <Link to="/" className="w-16 h-16 rounded-full flex items-center justify-center">
-	        <img alt="" src={logo} className="w-36 h-36 translate-y-2 max-w-none" />
+        <Link
+          to="/"
+          className="w-16 h-16 rounded-full flex items-center justify-center"
+        >
+          <img
+            alt=""
+            src={logo}
+            className="w-36 h-36 translate-y-2 max-w-none"
+          />
         </Link>
+        <HeaderText noBold fontSize="italic text-2xl" className="pb-2pr">
+          Open Doors Sanctuary
+        </HeaderText>
 
         <nav className="ml-auto flex justify-end self-center items-center gap-x-8">
           <button>Language</button>
