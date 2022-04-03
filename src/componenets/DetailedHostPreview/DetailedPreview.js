1cc089f85cd5e90689ee663fa55c030f6e020ecc
Dima <omeluan.dima@gmail.com>
2022-04-03 09:35:03 +0300

    Fix bug with openning detailed preview

--- src/componenets/DetailedHostPreview/DetailedPreview.js
+++ src/componenets/DetailedHostPreview/DetailedPreview.js
@@ -10,23 +10,27 @@ export default function DetailedPreview({onClick, host}) {
     return (
         <div className='preview'>
             <img className='close-in-preview' src={close} onClick={onClick} />
-            <h1 className='titleInDetailedPreview'>{host.children.title}</h1>
-            <h2 style={{marginBottom: "40px", textIndent: "2.5em"}}>{host.children.location}</h2>
+            <h1 className='titleInDetailedPreview'>{host.title}</h1>
+            <h2 style={{marginBottom: "40px", textIndent: "2.5em"}}>{host.location}</h2>
             <div className='container-long-tag'>
-                { host.children.short_term ? <LongTag>Short Term</LongTag> : null}
-                { host.children.long_term ? <LongTag>Long Term</LongTag> : null}
-                { host.children.kid_friedly ? <LongTag>Kids-friendly</LongTag> : null}
-                { host.children.pet_friendly ? <LongTag>Pets-friendly</LongTag> : null}
+                { host.short_term ? 
+                    <LongTag>Short Term</LongTag> : null}
+                { host.long_term ? 
+                    <LongTag>Long Term</LongTag> : null}
+                { host.kid_friedly ? 
+                    <LongTag>Kids-friendly</LongTag> : null}
+                { host.pet_friendly ? 
+                    <LongTag>Pets-friendly</LongTag> : null}
             </div>
             <h1 style={{textIndent: "2.5em", marginTop: "30px"}} className='detailsInDetailedPreview'>
                 Details
             </h1>
-            <div className='descriptionInDetailedPreview' >{host.children.description}</div>
+            <div className='descriptionInDetailedPreview' >{host.description}</div>
             <div className='infoButtonInDetailedPreview' onClick={() => setShowEmail(!showEmail)}>
-                {showEmail ? host.children.email : 'Email'}
+                {showEmail ? host.email : 'Email'}
             </div>
             <div className='infoButtonInDetailedPreview' onClick={() => setShowPhone(!showPhone)}>
-                {showPhone ? host.children.phone : 'Phone'}
+                {showPhone ? host.phone : 'Phone'}
             </div>
         </div>
     );
--- src/componenets/ShortHostPriview/HostShortForm.js
+++ src/componenets/ShortHostPriview/HostShortForm.js
@@ -2,23 +2,30 @@ import React, {useState} from 'react'
 import ShortTag from '../DetailedHostPreview/ShortTag';
 import DetailedPreview from '../DetailedHostPreview/DetailedPreview';
 
-export default function HostShortForm(host) {
+export default function HostShortForm({isBlocked, block, host}) {
     const [detailedPreview, setDetailedPreview] = useState(false)
 
     const close = () => {
+        block(false);
         setDetailedPreview(false);
+    }
+
+    const open = () => {
+        if(isBlocked()) return;
+        block(true);
+        setDetailedPreview(true);
     }
 
     return (
         <>
-            <div className='hostShortType' onClick={() => setDetailedPreview(true)}>
-                <h1 className='title'>{host.children.title}</h1>
-                <h2 className='location'>{host.children.location}</h2>
+            <div className='hostShortType' onClick={open}>
+                <h1 className='title'>{host.title}</h1>
+                <h2 className='location'>{host.location}</h2>
                 <div className='container'>
-                    { host.children.short_term ? <ShortTag>Short</ShortTag> : null}
-                    { host.children.long_term ? <ShortTag>Long</ShortTag> : null}
-                    { host.children.kid_friedly ? <ShortTag>Kids</ShortTag> : null}
-                    { host.children.pet_friendly ? <ShortTag>Pets</ShortTag> : null}
+                    { host.short_term ? <ShortTag>Short</ShortTag> : null}
+                    { host.long_term ? <ShortTag>Long</ShortTag> : null}
+                    { host.kid_friedly ? <ShortTag>Kids</ShortTag> : null}
+                    { host.pet_friendly ? <ShortTag>Pets</ShortTag> : null}
                 </div>
             </div>
 
