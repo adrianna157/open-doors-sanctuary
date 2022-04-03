2f6f65b9ecf74a564f317fad2d8cb318d94cc6ad
Dichik <71069933+Dichik@users.noreply.github.com>
2022-04-03 19:27:50 +0300

    Revert "Made a search and hosting list"

--- src/pages/GuestDashboard/index.js
+++ src/pages/GuestDashboard/index.js
@@ -1,28 +1,16 @@
-import { useState } from "react";
 import HeaderText from "../../components/HeaderText";
 import SearchBar from "../../components/SearchBar"
-import HostingList from '../../components/DetailedHostPreview/HostingList';
-import hosts from '../../assets/data/hosts.json'
-import styles from '../../components/DetailedHostPreview/local.module.css'
-
 export default function GuestDashboard() {
-    const [filter, setFilter] = useState('');
-
-    const updateHosts = (location) => {
-      setFilter(location)
-    } 
-
     return (
       <div className="min-h-screen flex items-center justify-center bg-hacky-50">
         <div>
-          <HeaderText noBold fontSize="text-4xl" className="text-center text-white pb-2pr">
+          <HeaderText noBold fontSize="text-4xl" className=" text-white pb-2pr">
             Discover an Open Door
             </HeaderText>
             <h3 className="text-white text-center mb-12 pr">
                 Helping The Crisis in Ukraine
             </h3>
-          <SearchBar onClick={updateHosts} />
-          <HostingList hostSummaries={filter ? hosts.filter( (host) => host.location.trim().toLowerCase().includes(filter.trim().toLowerCase()) ) : hosts} />
+          <SearchBar />
         </div>
       </div>
     );
